cfd4ed46f51752d869760212e56d10a34225b330
Clyde D'Souza <clyd94@gmail.com>
2019-07-28 11:49:04 +1200

    Add labels option to nunit command

--- SpecflowCmdUnitTestProject/RunTests.bat
+++ SpecflowCmdUnitTestProject/RunTests.bat
@@ -1,14 +1,14 @@
 @ECHO off
 
 ECHO What test would you like to run?
-ECHO 1 - All
-ECHO 2 - Regression only
+ECHO     1 - All
+ECHO     2 - Regression only
 SET /p choice= "Enter a number from the options above here: " 
 
 IF %choice%==1 (
-	.\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe  .\bin\Debug\SpecflowCmdUnitTestProject.dll --work=.\..\TestResults\
+	.\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe  .\bin\Debug\SpecflowCmdUnitTestProject.dll --work=.\..\TestResults\ --labels=All 
 ) else (
-	.\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe  .\bin\Debug\SpecflowCmdUnitTestProject.dll --where=cat==regression --work=.\..\TestResults\
+	.\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe  .\bin\Debug\SpecflowCmdUnitTestProject.dll --where=cat==regression --work=.\..\TestResults\ --labels=All 
 )
 
 PAUSE 
