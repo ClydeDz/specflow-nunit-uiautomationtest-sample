ee37c93a7aa3fca1168f086aed99d40295acc0d8
Clyde D'Souza <clyd94@gmail.com>
2019-07-28 23:17:46 +1200

    Add Extent CLI tool to convert nunit xml to html report

--- SpecflowCmdUnitTestProject/RunTests.bat
+++ SpecflowCmdUnitTestProject/RunTests.bat
@@ -11,4 +11,6 @@ IF %choice%==1 (
 	.\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe  .\bin\Debug\SpecflowCmdUnitTestProject.dll --where=cat==regression --work=.\..\TestResults\ --labels=All 
 )
 
+.\..\packages\extent.0.0.3\tools\extent.exe -i .\..\TestResults\TestResult.xml -o .\..\TestResults\HtmlOutput\
+
 PAUSE 
